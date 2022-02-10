f3bf06fce6ad9cf96fb5e3950b214ba53f5221bd
candon001 <catalin.andon@pwc.com>
2022-02-10 15:49:51 +0200

    adding new model

--- DVT_api/models.py
+++ DVT_api/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
+
+
+class Detailed_User(models.Model):
+    ppiCode = models.CharField(max_length=120)
+    username = models.CharField(max_length=60)
+    addedDate = models.DateTimeField('Added Date',max_length=120, blank =True)
+    email_address = models.EmailField('Email Address', max_length=120) 
+  
+
 class Users(models.Model):
     requestedApp = models.CharField(max_length=60) #change the lenght
-    username = models.CharField(max_length=60)
-    teamName = models.CharField(max_length=60)
     guid = models.CharField(max_length=60)
+    name = models.ForeignKey(Detailed_User, blank=True, null=True)
+    teamName = models.CharField(max_length=60)
     wbsCode = models.CharField(db_column='pwc_ppi', max_length=60)  
     ticketNumber = models.CharField(max_length=60)
-    email_address = models.CharField(max_length=120)
     accountType = models.CharField(max_length=120)
+    
 
    
 #modify the serializers, views , forms
