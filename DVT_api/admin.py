97fe371173b13dc3b57144c569dd09d139c574db
candon001 <catalin.andon@pwc.com>
2022-03-21 16:49:23 +0200

    added corrections

--- DVT_api/admin.py
+++ DVT_api/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import Atlassian, AzureDevOps, EventApp, GitHub, PwC_User
+from .models import EventApp
 
 
 admin.site.register(EventApp)
-admin.site.register(GitHub)
-admin.site.register(AzureDevOps)
-admin.site.register(Atlassian)
-admin.site.register(PwC_User)
+
